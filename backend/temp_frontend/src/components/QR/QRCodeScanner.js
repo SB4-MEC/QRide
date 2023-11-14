25d8ae48aa32f118feb58fb4c13cac9faeebb6ed
diasgt123 <diasgt123@gmail.com>
2023-11-14 22:42:31 +0530

    changes in qr scanner page

--- backend/temp_frontend/src/components/QR/QRCodeScanner.js
+++ backend/temp_frontend/src/components/QR/QRCodeScanner.js
@@ -8,46 +8,51 @@ import bgdot1 from "../Assets/bgdot1.png";
 const QRCodeScanner = () => {
   const navigate = useNavigate();
   const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      const scannedText = result.getText();
-      setResult(scannedText);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [busStops, setBusStops] = useState([
+    "Edappally",
+    "Thrikkakara",
+    "Pipeline",
+    "NGO",
+    "CUSAT",
+    // Add more bus stops as needed
+  ]);
+  const [selectedDestination, setSelectedDestination] = useState("");
 
-      // Check if the scanned text is "Edappally"
-      if (scannedText.toLowerCase() === "http://edapally") {
-        navigate("/register");
-        } 
-         else if (scannedText.toLowerCase() === "http://thrikkakara") {
-        navigate("/register");
-         }
-         else if (scannedText.toLowerCase() === "http://pipeline") {
-        navigate("/register");
-         }
-         else if (scannedText.toLowerCase() === "http://ngo") {
-        navigate("/register");
-         }
-         else if (scannedText.toLowerCase() === "http://cusat") {
-        navigate("/register");
-         }
-        // else if (isValidURL(scannedText)) {
-        // Redirect to the scanned URL if it's a valid URL
-        // window.location.href = scannedText;
-    //   } 
-      else {
-        // Handle other cases (non-URL, non-"Edappally") as needed
-        alert(`Scanned: ${scannedText}.Please scan the correct QR code`);
-      }
-    },
-  });
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-  // Function to check if a given string is a valid URL
-  const isValidURL = (url) => {
-    try {
-      new URL(url);
-      return true;
-    } catch (error) {
-      return false;
-    }
+  const handleDestinationSelect = (stop) => {
+    setSelectedDestination(stop);
+    setDropdownOpen(false);
+
+    // Now you can send the data to the backend or perform any other actions
+    // For example, call a function to fetch buses based on current location and selected destination
+    fetchBuses(result, stop);
+  };
+
+  const fetchBuses = (currentLocation, selectedDestination) => {
+    // Make a request to the backend API with the necessary data
+    // Example using fetch API
+    fetch('/api/track-buses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        currentLocation,
+        selectedDestination,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the backend
+        console.log('Buses:', data);
+      })
+      .catch(error => {
+        console.error('Error fetching buses:', error);
+      });
   };
 
   let backgroundImageUrl = `url(${bgdot})`;
@@ -65,6 +70,30 @@ const QRCodeScanner = () => {
     alignItems: "center",
   };
 
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      const scannedText = result.getText();
+      setResult(scannedText);
+
+      if (isValidURL(scannedText)) {
+        // Handle other cases (non-URL, non-"Edappally") as needed
+        alert(`Scanned: ${scannedText}. Please scan the correct QR code`);
+      } else {
+        // If the scanned QR is correct, initiate the dropdown menu
+        toggleDropdown();
+      }
+    },
+  });
+
+  const isValidURL = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
   return (
     <div className="background" style={backgroundStyle}>
       <div className="video-container">
@@ -72,6 +101,21 @@ const QRCodeScanner = () => {
         <p>
           <span className="qr-text">Scan your QR!</span>
         </p>
+        <div className="dropdown-container">
+          <button onClick={toggleDropdown} className="dropdown-button">
+            Select Destination
+          </button>
+
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              {busStops.map((stop, index) => (
+                <a key={index} onClick={() => handleDestinationSelect(stop)}>
+                  {stop}
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
